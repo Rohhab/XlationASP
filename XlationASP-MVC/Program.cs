63df7055c29e219213b2958a0986199bbf061f70
Rohhab <s.pooria.hosseini@gmail.com>
2025-01-05 21:16:20 +0330

    UPDATE - Implemented Google OAuth for user login

--- XlationASP-MVC/Program.cs
+++ XlationASP-MVC/Program.cs
@@ -39,6 +39,12 @@ builder.Services.AddAuthorizationBuilder()
     .RequireAuthenticatedUser()
     .Build());
 
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
 var app = builder.Build();
 
 // Create scope and initialize the database with SeedData
